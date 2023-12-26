706439070ade4e5477f48b3097a6c6a703764e9c
shravyabuddhi <41285320+shravyabuddhi@users.noreply.github.com>
2023-12-26 18:32:49 +0530

    Add files via upload
    First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const gif1 = document.getElementById("gif1");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yayyy, see you on the February 28th, 2024!";
+  let hidden = gif1.getAttribute("hidden");
+  gif.src =
+    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  gif1.removeAttribute("hidden");
+  yesBtn.style.display = "none"; 
+  noBtn.style.display = "none";  
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
